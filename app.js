ae19b35b187fe15ebe6153d67a192f23acaf1258
recallwolf <826628485@qq.com>
2017-09-26 22:09:39 +0800

    FrontEnd Socket.io Config

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 	socket.emit('open');
   	socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    	io.emit('chat message', msg);
   });
 });
 
