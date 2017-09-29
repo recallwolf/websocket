43162bd97354627f34f91b641b360fbd6827c7d4
recallwolf <826628485@qq.com>
2017-09-29 10:57:31 +0800

    Update BackEnd
    fix online user count bug
    fix socket.name value bug
    fix resolution bug

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var onlineUser = [];
 var num = 0;
 
 io.on('connection', function(socket){
-	num++;
 	socket.on('online',function(msg){
+		num++;
 		socket.name = msg;
 		io.emit('online', msg,num);
 	});
@@ -27,15 +27,17 @@ io.on('connection', function(socket){
     	io.emit('onlineuser',onlineUser);
   	});
   	socket.on('disconnect', function(){
-  		if(onlineUser!=[]&&num >= 0){
-  			num--;
-  			var index = onlineUser.findIndex(function(value){
-  				return  value.nickname === socket.name;
-  			});
-  			onlineUser.splice(index, 1);
-  			console.log(onlineUser);
-  		}
-  		socket.broadcast.emit('userleft', socket.name,onlineUser,num);
+  		num--;
+  		if(socket.name != null){
+  			if(onlineUser!=[]&&num >= 0){
+  				var index = onlineUser.findIndex(function(value){
+  					return  value.nickname === socket.name;
+  				});
+  				onlineUser.splice(index, 1);
+  				console.log(onlineUser);
+  			}
+			socket.broadcast.emit('userleft', socket.name,onlineUser,num);
+		}
   	});
 });
 
