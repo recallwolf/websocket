bc1224d6a7cc532f270325ea4c1b9ba96cad3f16
recallwolf <826628485@qq.com>
2017-10-03 14:46:30 +0800

    Typing
    show typing

--- app.js
+++ app.js
@@ -20,33 +20,45 @@ io.on('connection', function(socket){
 		socket.name = msg;
 		io.emit('online', msg,num);
 	});
-  	socket.on('chat message', function(msg,nickname){
-    	io.emit('chat message', msg,nickname);
-  	});
-  	socket.on('onlineuser', function(nickname){
-  		onlineUser.push({nickname: nickname});
-    	io.emit('onlineuser',onlineUser);
-  	});
-  	socket.on('sendfile', function(nickname,file,filename){
-      var rawPath = './public/files/' + filename;
-  		var filePath = path.resolve(__dirname, rawPath);
-      console.log(filePath);
-  		fs.writeFileSync(filePath, file, 'binary');
-  		io.emit('sendfile', nickname, filename);
-  	});
-  	socket.on('disconnect', function(){
-  		num--;
-  		if(socket.name != null){
-  			if(onlineUser!=[]&&num >= 0){
-  				var index = onlineUser.findIndex(function(value){
-  					return  value.nickname === socket.name;
-  				});
-  				onlineUser.splice(index, 1);
-  				console.log(onlineUser);
-  			}
-			socket.broadcast.emit('userleft', socket.name,onlineUser,num);
+
+  socket.on('chat message', function(msg,nickname){
+    io.emit('chat message', msg,nickname);
+  });
+
+  socket.on('typing', function(nickname){
+    io.emit('typing', nickname);
+  });
+
+  socket.on('stopTyping', function(nickname){
+    io.emit('stopTyping', nickname);
+  });
+
+  socket.on('onlineuser', function(nickname){
+  	onlineUser.push({nickname: nickname});
+    io.emit('onlineuser',onlineUser);
+  });
+
+  socket.on('sendfile', function(nickname,file,filename){
+    var rawPath = './public/files/' + filename;
+  	var filePath = path.resolve(__dirname, rawPath);
+    console.log(filePath);
+  	fs.writeFileSync(filePath, file, 'binary');
+  	io.emit('sendfile', nickname, filename);
+  });
+
+  socket.on('disconnect', function(){
+  	num--;
+  	if(socket.name != null){
+  		if(onlineUser!=[]&&num >= 0){
+  			var index = onlineUser.findIndex(function(value){
+  			return  value.nickname === socket.name;
+  			});
+  			onlineUser.splice(index, 1);
+  			console.log(onlineUser);
+  		}
+		  socket.broadcast.emit('userleft', socket.name,onlineUser,num);
 		}
-  	});
+  });
 });
 
 http.listen(3000, function(){
